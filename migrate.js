0a99efc662126e25ee011bca2773994d438a0282
kosinix <kosinix@users.noreply.github.com>
2023-01-10 14:22:14 +0800

    Move config to cli

--- migrate.js
+++ migrate.js
@@ -13,20 +13,24 @@ const get = require('./src/get')
 const { client1, client2 } = require('./src/clients')
 const { migrate, downloadObjects, deleteObjects } = require('./src/helpers')
 
-const MaxKeys = get(process, 'env.MaxKeys', 2)
+const BUCKET1 = get(process, 'env.BUCKET1', 'codefleet-hris-storage')
+const PREFIX1 = get(process, 'env.PREFIX1', 'files-live')
+const MAX_KEYS = get(process, 'env.MAX_KEYS', 2)
+const BUCKET2 = get(process, 'env.BUCKET2', 'hris-gsu-ph')
+
 
     ;
 (async () => {
     try {
 
         let objects = await downloadObjects(client1, new ListObjectsCommand({
-            Bucket: 'codefleet-hris-storage',
-            Prefix: 'files-dev',
-            MaxKeys: MaxKeys
+            Bucket: BUCKET1,
+            Prefix: PREFIX1,
+            MaxKeys: MAX_KEYS
         }))
         // console.log('objects:', objects)
         let results = await migrate(objects, client2, {
-            Bucket: 'hris-gsu-ph',
+            Bucket: BUCKET2,
         })
         console.log('migrated:', results)
 
